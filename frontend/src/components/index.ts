2d4fed291ceb139d6d90aeb6ba1ee017a1255bce
theRex <olosun.m1904582@st.futminna.edu.ng>
2026-01-25 01:52:00 +0100

    Add barrel export files for utils and components

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Components barrel export
+ */
+export { WalletConnect } from './WalletConnect';
+export { DepositCard } from './DepositCard';
+export { BorrowCard } from './BorrowCard';
+export { RepayCard } from './RepayCard';
+export { HealthMonitor } from './HealthMonitor';
+export { Dashboard } from './Dashboard';
+export { StatsCard } from './StatsCard';
+export { TransactionHistory } from './TransactionHistory';
+export { LiquidationList } from './LiquidationList';
